28deee73dbaab00ef2a24256a3e9ffaebfb8e2fa
MauroDegaspari <mauro_degaspari@hotmail.com>
2021-03-11 14:02:50 -0300

    Implementaçao de Timer com javaScript

--- src/components/Tempo.tsx
+++ src/components/Tempo.tsx
@@ -1,23 +1,84 @@
+import { useState, useEffect } from 'react';
 import styles from '../styles/components/Tempo.module.css'
 
+
+let paraTempo: NodeJS.Timeout;
+
 export function Tempo(){
+
+    /* Javascript do timer */
+    const [time, setTime] = useState(0.1 * 60);
+    const [isAtivo, setIsAtivo] = useState(false);
+    const [hasTermino, setHastermino] = useState(false);
+
+    const minutos = Math.floor(time / 60);
+    const segundo = time % 60;
+
+    const [minutoLeft, minutoRight] = String(minutos).padStart(2, '0').split('');
+    const [segundoLeft, segundoRight] = String(segundo).padStart(2, '0').split('');
+
+    
+
+    /* Funções de ações para o botão */
+    function Start(){
+        setIsAtivo(true);
+    }
+
+    function Restart(){
+        clearTimeout(paraTempo);
+        setIsAtivo(false);
+        setTime(25 * 60);
+    }
+
+    useEffect( () => {
+        if (isAtivo && time > 0 ){
+           paraTempo = setTimeout(() => {
+                setTime(time -1)
+            }, 1000)
+        } else if(setIsAtivo && time === 0){
+            setHastermino(true);
+            setIsAtivo(false);
+        }
+
+    }, [isAtivo, time]) 
+
     return(
     <div>
         <div className={styles.tempoContainer}>
             <div>
-                <span>2</span>
-                <span>5</span>
+                <span>{minutoLeft}</span>
+                <span>{minutoRight}</span>
             </div>
             <span>:</span>
             <div>
-                <span>0</span>
-                <span>0</span>
+                <span>{segundoLeft}</span>
+                <span>{segundoRight}</span>
             </div>
         </div>
 
-        <button type="button" className={styles.tempoButton}>
-            tempo inical
-        </button>        
+        { hasTermino ? (
+        <button disabled className={styles.tempoButton} >
+            
+               Ciclo concluido
+         </button> 
+    ) :
+        (
+            <>
+                  { isAtivo ? (
+                <button type="button" className={`${styles.tempoButton} ${styles.tempoButtonAtivo}`} onClick={Restart}>
+                Abandonar ciclo
+                </button> )
+                :
+                (<button type="button" className={styles.tempoButton} onClick={Start}>
+                Inicio ciclo
+              </button>)
+            }
+            </>
+        )
+    } 
+        
+
+               
 
     </div>
     );
